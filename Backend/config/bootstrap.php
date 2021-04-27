19825c7685c7c90c164415f7549bfe9b75cff2ba
Juan Rodas <juan.rodas.manez@gmail.com>
2021-04-27 00:32:55 -0500

    feat: Refacto Containers

--- Backend/config/bootstrap.php
+++ Backend/config/bootstrap.php
@@ -1,7 +1,12 @@
 <?php declare(strict_types=1);
 
+use App\Shared\Application\Slim\Handlers\HttpErrorHandler;
+use App\Shared\Application\Slim\Handlers\ShutdownHandler;
+use App\Shared\Application\Slim\ResponseEmitter\ResponseEmitter;
 use DI\ContainerBuilder;
+use Psr\Log\LoggerInterface;
 use Slim\App;
+use Slim\Factory\ServerRequestCreatorFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -11,18 +16,44 @@ $containerBuilder->addDefinitions(__DIR__ . '/container.php');
 
 try {
 
+    // Build PHP-DI Container instance
     $container = $containerBuilder->build();
 
     $app = $container->get(App::class);
+    $callableResolver = $app->getCallableResolver();
+    $displayErrorDetails = false;
 
-    (require __DIR__ . '/routes.php') ($app);
-
+    // Register Middleware
     (require __DIR__ . '/middleware.php') ($app);
 
-    return $app->run();
+    // Register Routes
+    (require __DIR__ . '/routes.php') ($app);
 
-} catch (Exception $e) {
+    // Create Request object from globals
+    $serverRequestCreator = ServerRequestCreatorFactory::create();
+    $request = $serverRequestCreator->createServerRequestFromGlobals();
 
+    // Create Error Handler
+    $responseFactory = $app->getResponseFactory();
+    $errorHandler = new HttpErrorHandler($callableResolver, $responseFactory, $container->get(LoggerInterface::class));
+
+    // Create Shutdown Handler
+    $shutdownHandler = new ShutdownHandler($request, $errorHandler, $displayErrorDetails);
+    register_shutdown_function($shutdownHandler);
+
+    // Add Error Middleware
+    $errorMiddleware = $app->addErrorMiddleware($displayErrorDetails, false, false);
+    $errorMiddleware->setDefaultErrorHandler($errorHandler);
+
+    // Run App & Emit Response
+    $response = $app->handle($request);
+    $responseEmitter = new ResponseEmitter();
+    $responseEmitter->emit($response);
+
+    // return $app->run();
+
+} catch (\DI\DependencyException $e) {
     throw new Exception($e->getMessage());
-
+} catch (\DI\NotFoundException $e) {
+    throw new Exception($e->getMessage());
 }
--- Backend/config/settings.php
+++ Backend/config/settings.php
@@ -1,1 +1,25 @@
 <?php declare(strict_types=1);
+
+use App\Shared\Application\Slim\Settings\Settings;
+use App\Shared\Application\Slim\Settings\SettingsInterface;
+use DI\ContainerBuilder;
+use Monolog\Logger;
+
+return function (ContainerBuilder $containerBuilder) {
+
+    // Global Settings Object
+    $containerBuilder->addDefinitions([
+        SettingsInterface::class => function () {
+            return new Settings([
+                'displayErrorDetails' => true, // Should be set to false in production
+                'logError'            => false,
+                'logErrorDetails'     => false,
+                'logger' => [
+                    'name' => 'slim-app',
+                    'path' => '/../logs/app.log',
+                    'level' => Logger::DEBUG,
+                ],
+            ]);
+        }
+    ]);
+};
